ae6d4c922a22bc793ff7f79fb23792aa7567aa2e
koseungsoon <koseungsoon@naver.com>
2024-02-20 12:11:38 +0900

    주석처리

--- src/pkg1/Test1.java
+++ src/pkg1/Test1.java
@@ -5,7 +5,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("hi2");
-		System.out.println("hi3");
+//		System.out.println("hi3");
+		
+		
 	}
 
 }
